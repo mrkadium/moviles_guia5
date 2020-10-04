20f6be7fe52ec6e57dc4158d86a023828ffa3677
Mario Olivo <mario_rivera.olivo@outlook.es>
2020-10-04 13:58:19 -0600

    Updated chat to have a click listener and a background color

--- app/src/main/java/com/uso/guia5/adapters/ChatAdapter.kt
+++ app/src/main/java/com/uso/guia5/adapters/ChatAdapter.kt
@@ -1,15 +1,20 @@
 package com.uso.guia5.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.uso.guia5.R
 import com.uso.guia5.models.Chat
 import com.uso.guia5.viewholders.ChatViewHolder
 
 class ChatAdapter(var data:List<Chat>) : RecyclerView.Adapter<ChatViewHolder>() {
+    private lateinit var context:Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        context = parent.context
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_chat, parent, false)
         return ChatViewHolder(view)
     }
@@ -19,8 +24,13 @@ class ChatAdapter(var data:List<Chat>) : RecyclerView.Adapter<ChatViewHolder>() 
         holder.vChatState.visibility = if(chat.state) View.VISIBLE else View.INVISIBLE
         holder.tvChatHour.text = chat.hour
         holder.tvChatNombre.text = chat.nombre
+        if(chat.state) holder.tvChatNombre.setTextColor(context.resources.getColor(R.color.colorPrimaryDark))
         holder.tvChatMensaje.text = chat.mensaje
         holder.ivChatImage.setImageResource(chat.image)
+
+        holder.cardviewChat.setOnClickListener{
+            Toast.makeText(context, "Clicked on: ".plus(chat.nombre), Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/uso/guia5/viewholders/ChatViewHolder.kt
+++ app/src/main/java/com/uso/guia5/viewholders/ChatViewHolder.kt
@@ -3,11 +3,13 @@ package com.uso.guia5.viewholders
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.uso.guia5.R
 import kotlinx.android.synthetic.main.item_chat.view.*
 
 class ChatViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    var cardviewChat:CardView = itemView.findViewById(R.id.cardChat)
     var vChatState:View = itemView.findViewById(R.id.vChatState)
     var tvChatHour:TextView = itemView.findViewById(R.id.tvChatHour)
     var ivChatImage: ImageView = itemView.findViewById(R.id.ivChatImage)
